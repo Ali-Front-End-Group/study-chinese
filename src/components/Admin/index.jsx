678f532ba4339b02ffb5efe234bc1d14599357d1
Jack <965555169@qq.com>
2021-11-14 18:04:08 +0800

    添加课程页面

--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -7,45 +7,79 @@ const Admin = () => {
     //测试数据
     const courseData = [
         {
-            imgLink: "/img/语文.jpeg",
-            courseName: "111",
-            teacherName: "222",
-            courseLink: "http://www.baidu.com"
+            imgLink: '/img/语文.jpeg',
+            courseName: '111',
+            teacherName: '222',
+            courseLink: 'http://www.baidu.com',
         },
         {
-            imgLink: "/img/数学.jpg",
-            courseName: "333",
-            teacherName: "444",
-            courseLink: "http://www.bilibili.com"
+            imgLink: '/img/数学.jpg',
+            courseName: '333',
+            teacherName: '444',
+            courseLink: 'http://www.bilibili.com',
         },
         {
-            imgLink: "/img/语文.jpeg",
-            courseName: "111",
-            teacherName: "222",
-            courseLink: "http://www.baidu.com"
+            imgLink: '/img/语文.jpeg',
+            courseName: '111',
+            teacherName: '222',
+            courseLink: 'http://www.baidu.com',
         },
         {
-            imgLink: "/img/数学.jpg",
-            courseName: "333",
-            teacherName: "444",
-            courseLink: "http://www.bilibili.com"
+            imgLink: '/img/数学.jpg',
+            courseName: '333',
+            teacherName: '444',
+            courseLink: 'http://www.bilibili.com',
         },
-    ]
+        {
+            imgLink: '/img/数学.jpg',
+            courseName: '333',
+            teacherName: '444',
+            courseLink: 'http://www.bilibili.com',
+        },
+        {
+            imgLink: '/img/数学.jpg',
+            courseName: '333',
+            teacherName: '444',
+            courseLink: 'http://www.bilibili.com',
+        },
+        {
+            imgLink: '/img/数学.jpg',
+            courseName: '333',
+            teacherName: '444',
+            courseLink: 'http://www.bilibili.com',
+        },
+        {
+            imgLink: '/img/数学.jpg',
+            courseName: '333',
+            teacherName: '444',
+            courseLink: 'http://www.bilibili.com',
+        },
+        {
+            imgLink: '/img/数学.jpg',
+            courseName: '333',
+            teacherName: '444',
+            courseLink: 'http://www.bilibili.com',
+        },
+        {
+            imgLink: '/img/数学.jpg',
+            courseName: '333',
+            teacherName: '444',
+            courseLink: 'http://www.bilibili.com',
+        },
+    ];
 
     return (
         <div className="courseLayout">
             <AddCourse />
-            {
-                courseData.map(course => (
-                    <CourseCard 
-                        key={course.courseName}
-                        imgLink={course.imgLink}
-                        courseName={course.courseName} 
-                        teacherName={course.teacherName}
-                        courseLink={course.courseLink}
-                    />
-                ))
-            }
+            {courseData.map(course => (
+                <CourseCard
+                    key={course.courseName}
+                    imgLink={course.imgLink}
+                    courseName={course.courseName}
+                    teacherName={course.teacherName}
+                    courseLink={course.courseLink}
+                />
+            ))}
         </div>
     );
 };
