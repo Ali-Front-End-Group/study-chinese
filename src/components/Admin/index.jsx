80336b87b76ca6c8c2046b31b0ca1ea65a624e7d
CC2m2 <389269570@qq.com>
2021-11-14 17:51:17 +0800

    update amdin

--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -1,14 +1,51 @@
 // import { useState, useEffect } from 'react';
 import CourseCard from './CourseCard';
+import AddCourse from './AddCourse';
 import './index.css';
 
 const Admin = () => {
+    //测试数据
+    const courseData = [
+        {
+            imgLink: "/img/语文.jpeg",
+            courseName: "111",
+            teacherName: "222",
+            courseLink: "http://www.baidu.com"
+        },
+        {
+            imgLink: "/img/数学.jpg",
+            courseName: "333",
+            teacherName: "444",
+            courseLink: "http://www.bilibili.com"
+        },
+        {
+            imgLink: "/img/语文.jpeg",
+            courseName: "111",
+            teacherName: "222",
+            courseLink: "http://www.baidu.com"
+        },
+        {
+            imgLink: "/img/数学.jpg",
+            courseName: "333",
+            teacherName: "444",
+            courseLink: "http://www.bilibili.com"
+        },
+    ]
+
     return (
         <div className="courseLayout">
-            <CourseCard />
-            <CourseCard />
-            <CourseCard />
-            <CourseCard />
+            <AddCourse />
+            {
+                courseData.map(course => (
+                    <CourseCard 
+                        key={course.courseName}
+                        imgLink={course.imgLink}
+                        courseName={course.courseName} 
+                        teacherName={course.teacherName}
+                        courseLink={course.courseLink}
+                    />
+                ))
+            }
         </div>
     );
 };
